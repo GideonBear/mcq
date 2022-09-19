ece9952941997cf98aa152e84e452de4a9e145ac
GideonBear <87426140+GideonBear@users.noreply.github.com>
2022-09-19 20:05:55 +0200

    Stuff

--- mcq/get.py
+++ mcq/get.py
@@ -11,20 +11,36 @@ def get(source_type, value) -> Path:
     return curr
 
 
-def default(version):
-    pass
+curseforge_vanilladefault_url = 'https://www.curseforge.com/minecraft/texture-packs/vanilladefault/download/{}/file'
+curseforge_vanilladefault = {
+
+}
+texturepackscom_url = 'https://texture-packs.com/default-texture-pack-1-{}-download/'
+texturepackscom_supported = '17 16 15 14 13 12 10 8'.split(' ')
+texturepackscom_other = {
+    '11': '12',
+    '9': '10'
+}
 
 
-def url(url):
-    pass
+def from_default(version: str):
+    version = version.removeprefix('1.')
+    raise NotImplementedError
 
 
-def file(path):
+def from_url(url: str):
+    raise NotImplementedError
+
+
+def from_file(path):
     path = Path(path).resolve()
+    if not path.exists():
+        raise NotImplementedError
+    raise NotImplementedError
 
 
 source_map = {
-    'default': default,
-    'url': url,
-    'file': file
+    'default': from_default,
+    'url': from_url,
+    'file': from_file
 }
