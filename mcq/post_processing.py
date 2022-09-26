416ba136438db23bf8baa31be86233b7ea88d1e8
Gideon <87426140+GideonBear@users.noreply.github.com>
2022-09-26 12:08:21 +0200

    Closes #4

--- mcq/post_processing.py
+++ mcq/post_processing.py
@@ -15,6 +15,7 @@ def post_process(path: Path, export_only_textures: bool, zip_: bool):
 
 def do_export_only_textures(path: Path):
     textures = path / TEXTURES_PATH
+    print('Deleting with blacklist...')
     process(path, 'delete', None, [], [textures, 'pack.mcmeta', 'pack.png'])
 
 
